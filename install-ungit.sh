7d4e5dfa2527961deb52385cb554f0108c7bca7d
snuz <snuz@snuz.ru>
2017-10-23 15:33:35 +0300

    update ungit install

--- install-ungit.sh
+++ install-ungit.sh
@@ -1,4 +1,5 @@
-sudo apt-get install nodejs-legacy nodejs npm
+sudo apt update
+sudo apt install nodejs npm
 npm config set prefix ~/npm
 sudo ln -s /usr/bin/nodejs /usr/bin/mode
 sudo -H npm install -g ungit
